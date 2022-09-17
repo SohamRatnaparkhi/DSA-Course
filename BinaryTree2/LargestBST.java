5aed4a3ef199fccaac7f72ac4980e2513726d244
SohamRatnaparkhi <soham.ratnaparkhi@gmail.com>
2022-09-17 13:12:53 +0530

    binary tree2

--- BinaryTree2/LargestBST.java
+++ BinaryTree2/LargestBST.java
@@ -6,22 +6,39 @@ public class LargestBST extends LevelwiseTree {
         printTree(root);
         printLevelWise(root);
         System.out.println("------------");
-        largestBST(root);
-        System.out.println(mh);
+        int x = largestBST(root);
+        System.out.println(x);
     }
 
-    static int mh = 0, smh = 0;
-
     private static int largestBST(BinaryTreeNode<Integer> root) {
-        if (root == null)
-            return 0;
-        int lh = largestBST(root.left);
-        int rh = largestBST(root.right);
-        int x = Math.max(lh, rh);
-        if (x > mh) {
-            smh = mh;
-            mh = x;
+        NodeDetails height = largestSubtree(root);
+        return height.height;
+    }
+
+    private static NodeDetails largestSubtree(BinaryTreeNode<Integer> root) {
+        if (root == null) {
+            return new NodeDetails(0, Integer.MIN_VALUE, Integer.MAX_VALUE);
         }
-        return 1 + x;
+
+        NodeDetails left = largestSubtree(root.left);
+        NodeDetails right = largestSubtree(root.right);
+
+        if (left.max < root.data && root.data < right.min) {
+            return new NodeDetails(1 + Math.max(left.height, right.height), right.max, left.min);
+        } else {
+            return new NodeDetails(Math.max(left.height, right.height), Integer.MAX_VALUE, Integer.MIN_VALUE);
+        }
+    }
+}
+
+class NodeDetails {
+    int height;
+    int max;
+    int min;
+
+    public NodeDetails(int height, int max, int min) {
+        this.height = height;
+        this.max = max;
+        this.min = min;
     }
 }
